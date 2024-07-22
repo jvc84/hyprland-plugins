0d0935640a16a18fd05f3e2d021e0d67d73e56a9
jvc84 <72560784+jvc84@users.noreply.github.com>
2024-07-22 02:06:34 +0000

    Update barDeco.cpp

--- hyprbars/barDeco.cpp
+++ hyprbars/barDeco.cpp
@@ -314,7 +314,7 @@ void CHyprBar::renderBarButtons(const Vector2D& bufferSize, const float scale) {
         const int RADIUS = static_cast<int>(std::ceil(scaledButtonSize / 2.0));
 
         cairo_set_source_rgba(CAIRO, button.col.r, button.col.g, button.col.b, button.col.a);
-        cairo_arc(CAIRO, X, Y + RADIUS, RADIUS, 0, 2 * M_PI);
+        cairo_rectangle(CAIRO, X - RADIUS, Y, RADIUS, RADIUS);
         cairo_fill(CAIRO);
 
         offset += scaledButtonsPad + scaledButtonSize;
